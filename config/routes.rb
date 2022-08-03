cb47f005c5047df5ee681f693634d09d52f7ca21
Natty0404 <106375669+Natty0404@users.noreply.github.com>
2022-08-03 04:43:01 +0000

    [Add] users,posts controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
+  get 'posts/new'
+  get 'posts/create'
+  get 'posts/index'
+  get 'posts/show'
+  get 'posts/edit'
+  get 'posts/update'
+  get 'posts/destroy'
+  get 'users/index'
+  get 'users/show'
+  get 'users/edit'
+  get 'users/update'
+  get 'users/unsubscribe'
+  get 'users/withdraw'
   root to: "homes#top"
   get "home/about"=>"homes#about"
 
