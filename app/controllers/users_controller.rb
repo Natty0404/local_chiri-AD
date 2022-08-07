bbacd69863e50a7bf3e7a4daf8a2f542e24db4f7
Natty0404 <106375669+Natty0404@users.noreply.github.com>
2022-08-07 04:08:58 +0000

    [Add] kaminari

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @posts = @user.posts
+    @posts = @user.post.page(params[:page]).per(10)
   end
 
   def edit
