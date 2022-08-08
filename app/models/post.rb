1852b268ea4d4911b3ceb263014ced8409895743
Natty0404 <106375669+Natty0404@users.noreply.github.com>
2022-08-08 04:09:38 +0000

    [Add] PostComments model

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,7 @@ class Post < ApplicationRecord
   has_one_attached :post_image
   belongs_to :user
   has_many :favorites, dependent: :destroy
+  has_many :post_comments, dependent: :destroy
 
   validates :title, presence: true
   validates :body, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,7 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
   has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :post_comments, dependent: :destroy
 
   # 一覧画面で使用
   has_many :followings, through: :relationships, source: :followed
