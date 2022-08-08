fe2bb363f5a21e2eebb1da0f04869f2cc33cca1e
Natty0404 <106375669+Natty0404@users.noreply.github.com>
2022-08-08 06:25:20 +0000

    [Add] Comments destroy

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -3,8 +3,14 @@ class PostCommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     @comment = current_user.post_comments.new(post_comment_params)
-    @comment.post_id = post_id
+    @comment.post_id = @post.id
     @comment.save
+    redirect_to request.referer
+  end
+
+  def destroy
+    @comment = PostComment.find_by(id: params[:id], post_id: params[:post_id])
+    @comment.destroy
     redirect_to request.referer
   end
 
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -25,6 +25,7 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
     @user = @post.user
     @post_comment = PostComment.new
+    @post_comments = @post.post_comments.page(params[:page]).per(3)
   end
 
   def edit
