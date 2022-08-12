610f88469f6fda90df22eb5bb948d2075a78bef1
Natty0404 <106375669+Natty0404@users.noreply.github.com>
2022-08-12 06:09:58 +0000

    [Fix] header

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,62 @@
+class Admin::UsersController < ApplicationController
+
+  def index
+    @users = User.all
+    @post = Post.new
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @posts = @user.posts.page(params[:page]).per(10)
+    # @favorites = Favorite.where(user_id: current_user.id).pluck(:post_id)
+    # @favorite_list = Post.find(favorites).page(params[:page]).per(10)
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to user_path(@user)
+    else
+      render 'edit'
+    end
+  end
+
+  def unsubscribe
+  end
+
+  def withdraw
+    @user = current_user
+    @user.update(is_deleted: true)
+    reset_session
+    flash[:notice] = "またのご利用お待ちしております"
+    redirect_to root_path
+  end
+
+  def favorite
+    @favorites = Favorite.where(user_id: current_user.id).pluck(:post_id)
+    @favorite_list = Post.find(@favorites)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :profile_image)
+  end
+
+  def ensure_correct_user
+    @user = User.find(params[:id])
+    unless @user == current_user
+      redirect_to user_path(current_user)
+    end
+  end
+
+  def ensure_guest_user
+    @user = User.find(params[:id])
+    if @user.name == "ゲストユーザー"
+      redirect_to user_path(current_user) , notice: "ゲストユーザーはプロフィール編集画面へ遷移できません"
+    end
+  end
+
+end
